f8fee752292dd082247a381c61e7bb43dddd069d
Adrian Camacho <adriancamacho18@gmail.com>
2021-06-02 14:19:19 -0700

    update js to seperate business and UI logic
    co-authored-by: Kevin Funez <kevin.funez315@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,9 @@
+// Business Logic:
+
 function add(number1, number2) {
   return number1 + number2;
 }
 
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-const result = divide(number1, number2);
-alert(result);
 
 function subtract(number1, number2) {
   return number1 - number2;
@@ -20,3 +18,15 @@ function multiply(number1, number2) {
 function divide(number1, number2) {
   return number1 / number2;
 }
+
+// User Interface Logic:
+
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+  const number1 = parseInt($("#add1").val());
+  const number2 = parseInt($("#add2").val());
+  const result = add(number1, number2);
+  $("#output").text(result);
+  });   
+});
